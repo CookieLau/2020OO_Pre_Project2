8b29d4799e3d51832833484436674b6aafe78b8c
CookieLau <1826772769@qq.com>
2019-08-10 17:49:50 +0800

    init task3

--- src/Pre_Project2.java
+++ src/Pre_Project2.java
@@ -1,3 +1,4 @@
+import java.text.DecimalFormat;
 import java.util.Iterator;
 import java.util.Scanner;
 
@@ -14,8 +15,12 @@ public class Pre_Project2 {
         }
         //System.out.println(container.getCount());
         Iterator<String> wordIter = container.getIter();
+        int sumCount = container.getSumCount();
+        DecimalFormat df = new DecimalFormat("#.00%");
         while (wordIter.hasNext()) {
-            System.out.println(wordIter.next());
+            word = wordIter.next();
+            String rate = df.format((float)container.getCount(word)/sumCount);
+            System.out.println(String.format("%s %d %s", word, container.getCount(word), rate));
         }
     }
 
@@ -24,6 +29,16 @@ public class Pre_Project2 {
         //因为要去掉最后的,和. 所以使用 StringBuffer类记录 (String是不可变字符串，StringBuilder线程不安全)
         if (scanner.hasNext()) {
             StringBuffer nextWord = new StringBuffer(scanner.next().toLowerCase());
+            //Task3: '-'
+            while (nextWord.charAt(nextWord.length() - 1) == '-') {
+                nextWord.deleteCharAt(nextWord.length() - 1);
+                if (!scanner.hasNext()) {
+                    System.err.println("- is at an end, which is an invalid case!");
+                    System.exit(0);
+                } else { //connect another word in the next line
+                    nextWord.append(scanner.next());
+                }
+            }
             int length = nextWord.length();
             int index = length - 1;
             while (nextWord.charAt(index) == ',' || nextWord.charAt(index) == '.') {
--- src/WordContainer.java
+++ src/WordContainer.java
@@ -1,23 +1,47 @@
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class WordContainer {
     private Set<String> container;
+    private Map<String, Integer> word2Count;
+    private int sumCount;
 
     public WordContainer() {
         container = new TreeSet<>();
+        word2Count = new HashMap<>();
+        sumCount = 0;
     }
 
     public boolean addWord(String string) {
         if (null == string || string.length() == 0) {
             return false;
         }
+        sumCount++;
+        if (word2Count.containsKey(string)) {
+            int counts = word2Count.remove(string) + 1;
+            word2Count.put(string, counts);
+            return true;
+        }
+        word2Count.put(string, 1);
         return container.add(string);
     }
 
-    public int getCount() {
+    public int getDistinctCount() {
         return container.size();
+    }
+
+    public int getSumCount() {
+        return sumCount;
+    }
+
+    public int getCount(String word) {
+        if (!word2Count.containsKey(word)) {
+            return -1;
+        }
+        return word2Count.get(word);
     }
 
     public Iterator<String> getIter() {
