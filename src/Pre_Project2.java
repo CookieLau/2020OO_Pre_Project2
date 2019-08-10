1fa1662e17e880d4751081ee3c105bcff9ce9713
CookieLau <30521971+CookieLau@users.noreply.github.com>
2019-08-10 23:32:11 +0800

    Update Pre_Project2.java

--- src/Pre_Project2.java
+++ src/Pre_Project2.java
@@ -1,5 +1,5 @@
 public class Pre_Project2 {
     public static void main(String[] args) {
-        System.out.println("Welcome to OO Course, 2018OO ers");
+        System.out.println("Welcome to OO Course, 2020OO ers");
     }
 }
